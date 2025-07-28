a3ab2353d93d204ef4481ca47b81f6ba36d7f994
yixiangyin <y1027020239@qq.com>
2025-07-28 21:07:21 +1000

    fix bug: persons not displayed

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -2,7 +2,7 @@ import Person from "./Person";
 
 const Persons = (props) => {
   return props.personsToShow.map((person) => {
-    <Person key={person.id} name={person.name} number={person.number} />;
+    return <Person key={person.id} name={person.name} number={person.number} />;
   });
 };
 
