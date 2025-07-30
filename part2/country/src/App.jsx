c1fabd21f93a99e59922fc3467e5499483ff3a02
yixiangyin <y1027020239@qq.com>
2025-07-30 22:21:54 +1000

    2.18

--- part2/country/src/App.jsx
+++ part2/country/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useEffect } from "react";
 const App = () => {
   const [value, setValue] = useState("");
-  const [countries, setCountries] = useState(null);
+  const [countries, setCountries] = useState([]);
   const [filtered, setFiltered] = useState([]);
 
   useEffect(() => {
@@ -15,19 +15,24 @@ const App = () => {
       });
   }, []);
 
-  if (!countries) {
-    return null;
-  }
-  const onChange = (event) => {
-    setValue(event.target.value);
+  // Run filtering when value or countries changes
+  useEffect(() => {
+    if (!value || countries.length === 0) {
+      setFiltered([]);
+      return;
+    }
+
     const matches = countries.filter((country) =>
-      country.name.common
-        .toLowerCase()
-        .includes(event.target.value.toLowerCase())
+      country.name.common.toLowerCase().includes(value.toLowerCase())
     );
     setFiltered(matches);
-    console.log("matches", matches);
-    console.log("matches length", matches.length);
+  }, [value, countries]);
+  // if (!countries) {
+  //   return null;
+  // }
+  // Handle input change
+  const onChange = (event) => {
+    setValue(event.target.value);
   };
   return (
     <div>
@@ -44,14 +49,18 @@ const App = () => {
         {filtered.length == 1 && (
           <div>
             <h1>{filtered[0].name.common}</h1>
-            <p>Capital {filtered[0].capital[0]}</p>
+            <p>Capital {filtered[0].capital?.[0] ?? "N/A"}</p>
             <p>Area {filtered[0].area}</p>
             <h2>Languages</h2>
             <ul>
               {Object.values(filtered[0].languages).map((l) => {
-                return <li>{l}</li>;
+                return <li key={l}>{l}</li>;
               })}
             </ul>
+            <img
+              src={filtered[0].flags["png"]}
+              alt={filtered[0].flags["alt"]}
+            />
           </div>
         )}
       </div>
