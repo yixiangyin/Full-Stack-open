60e9761b342ff637de94ebebecdc33152ea3ffac
yixiangyin <y1027020239@qq.com>
2025-08-02 16:04:39 +1000

    3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -45,6 +45,12 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+    const id = req.params.id
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
